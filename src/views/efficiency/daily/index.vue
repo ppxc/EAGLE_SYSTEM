62f2b4a672afbe83ebcdf98ce2de8561cc343e95
123456789987654321000000 <2597861512@qq.com>
2026-04-15 13:22:37 +0800

    add table

---
+++ src/views/efficiency/daily/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div style="padding: 20px;">
+    <el-card>
+      <h3>日常通报</h3>
+      <!-- 这里可以写你要展示的内容 -->
+    </el-card>
+  </div>
+</template>
+
+<script setup lang="ts">
+// 日常通报页面逻辑
+</script>
