57665983b4a3f59f2c5bf0fd3967d27d0af87310
ppxc <vmct104674@163.com>
2026-04-15 16:36:57 +0800

    add global_refresh.ts in plugins and solve 'ResizeObserver loop completed with undelivered notifications' Error on App.vue

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import '@styles/index.scss'                         // 样式
 //import '@utils/sys/console.ts'                      // 控制台输出内容
 import { setupGlobDirectives } from './directives'
 import { setupErrorHandle } from './utils/sys/error-handle'
+import { GlobalAutoRefresh } from './plugins/globalAutoRefresh'
 import "@utils/ui/iconify-loader";
 
 document.addEventListener(
@@ -16,11 +17,24 @@ document.addEventListener(
   { passive: false }
 )
 
+
+
+
 const app = createApp(App)
+
 initStore(app)
 initRouter(app)
 setupGlobDirectives(app)
 setupErrorHandle(app)
-
+app.use(GlobalAutoRefresh, {
+  interval: 30,//30分钟刷新一次
+  enabled: true,
+  // callback: () => {
+  //   console.log('自动刷新操作');
+  // }
+})
+// app.use(store)
+// app.use(router)
 app.use(language)
+
 app.mount('#app')
